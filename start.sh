00f14b3351881b62b4b6118e759ee2ee91970d69
ROOT <root@comsource.cz>
2018-11-06 14:14:26 +0100

    chown lib and log directory  on start

--- start.sh
+++ start.sh
@@ -6,5 +6,7 @@ ln -sf /etc/sunstone-views /etc/one/sunstone-views
 ssh-keygen -A
 /usr/sbin/sshd -E /var/log/one/sshd.log
 
+chown oneadmin:oneadmin /var/log/one /var/lib/one
+
 su oneadmin /start-as-oneadmin.sh
 tail -F /var/log/one/*.{log,errror}
